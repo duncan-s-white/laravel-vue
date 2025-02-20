c8c29b61cc408337901033606a6d05cc16b434f0
Duncan White <duncanstanleywhite@gmail.com>
2025-02-20 09:42:23 +0000

    update app table migration

--- database/migrations/2025_02_18_105402_create_apps_table.php
+++ database/migrations/2025_02_18_105402_create_apps_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,6 +17,8 @@ return new class extends Migration
             $table->integer('appid');
             $table->string('name');
         });
+
+        $this->insert_apps();
     }
 
     /**
@@ -24,5 +27,25 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('apps');
+    }
+
+    private function insert_apps()
+    {
+
+        define("MAX_RECORDS", 10000);
+        define("CHUNK_SIZE", 1000);
+
+        $string = file_get_contents("./game-json-data-edit.json");
+        $apps = json_decode($string, true)["apps"];
+
+        $count = 0;
+        foreach (array_chunk($apps, CHUNK_SIZE) as $chunk) {
+            $count += CHUNK_SIZE;
+            DB::table('apps')->insert($chunk);
+            if ($count >= MAX_RECORDS) break;
+        }
+
+        DB::table('apps')->where('name', '=', '')->delete();
+        DB::table('apps')->where('name', 'like', '%test%')->delete();
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,8 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->create_users();
-
-        $this->insert_apps();
     }
 
     private function create_users()
@@ -27,25 +24,5 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
-    }
-
-    private function insert_apps()
-    {
-
-        define("MAX_RECORDS", 10000);
-        define("CHUNK_SIZE", 1000);
-
-        $string = file_get_contents("./game-json-data-edit.json");
-        $apps = json_decode($string, true)["apps"];
-
-        $count = 0;
-        foreach (array_chunk($apps, CHUNK_SIZE) as $chunk) {
-            $count += CHUNK_SIZE;
-            DB::table('apps')->insert($chunk);
-            if ($count >= MAX_RECORDS) break;
-        }
-
-        DB::table('apps')->where('name', '=', '')->delete();
-        DB::table('apps')->where('name', 'like', '%test%')->delete();
     }
 }
